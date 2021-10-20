8ef9c3bc904cacd1cb69f77a6e6cc4d155c4d94a
Alexandre Kimura <alexandremkimura@hotmail.com>
2021-10-19 21:39:46 -0300

    Create function Guest with SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { GetServerSideProps } from 'next';
 import { FormEvent, useContext, useState } from 'react'
 import { parseCookies } from 'nookies';
 import { AuthContext } from '../contexts/AuthContext';
+import { withSSRGuest } from '../utils/withSSRGuest';
 //import styles from '../styles/'
 
 export default function Home() {
@@ -31,20 +32,8 @@ export default function Home() {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-
-  const cookies = parseCookies(ctx)
-
-  if(!cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false
-      }
-    }
-  }
-
+export const getServerSideProps = withSSRGuest(async (ctx) => {
   return {
     props: {}
   }
-}
+});
